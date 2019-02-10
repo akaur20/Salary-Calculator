5f252e0bc058a3b08b0ad3bb14449885223c5004
wizleen <wizleen@AVLEEN.fios-router.home>
2019-02-10 15:34:00 -0500

    final

--- src/SalaryCalculator.java
+++ src/SalaryCalculator.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JCheckBox;
 public class SalaryCalculator 
 {
 
@@ -35,18 +36,35 @@ public class SalaryCalculator
 		hweek.setBounds(20, 80, 100, 30);
 		salCal.add(hweek);
 		
-		JLabel amount = new JLabel("Amount $ ");
-		amount.setBounds(140, 140, 100, 30);
+		JLabel amount = new JLabel("Annual Salary: $ ");
+		amount.setBounds(140, 160, 300, 30);
 		salCal.add(amount);
+		
+		JLabel fullT = new JLabel("Full Time");
+		fullT.setBounds(20,120, 100, 30);
+		salCal.add(fullT);
+		
+		JCheckBox fullTime = new JCheckBox();
+		fullTime.setBounds(100, 120, 30, 30);
+		salCal.add(fullTime);
 		
 		JButton calculate = new JButton("Calculate");
 		calculate.setSize(new Dimension(100,30));
-		calculate.setLocation(new Point(20,140));
+		calculate.setLocation(new Point(20,160));
 		calculate.addActionListener(new ActionListener()
 				{
 					public void actionPerformed(ActionEvent e)
 					{
 						
+						if(fullTime.isSelected())
+						{
+							hours.setText(null);
+							amount.setText("Annual Salary: $ " + Double.parseDouble(rate.getText()) * 40 * 52);
+						}
+						else
+						{
+							amount.setText("Annual Salary: $ " + Double.parseDouble(hours.getText()) * Double.parseDouble(rate.getText()) * 52);
+						}
 						
 					}
 				});
